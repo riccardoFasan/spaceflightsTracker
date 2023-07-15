87e69bb070fcae9aa8840e39adc0b1b847e75058
richard <fasanriccardo21@gmail.com>
2023-07-15 11:55:03 +0200

    refactor: layout folder

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,4 @@
-export * from "./BottomNavigationBar";
 export * from "./ScrollableList";
 export * from "./ButtonSecondary";
-export * from "./NavigationButton";
 export * from "./ListCard";
 export * from "./Countdown";
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,3 +1,4 @@
 export * from "./common";
 export * from "./features";
 export * from "./pages";
+export * from "./layout";
